164f619d4028d91abaedae503514325f2d361d56
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-11-06 20:28:18 +0000

    test: Added Typography tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Configure jest-axe to use vitest's expect
+import { toHaveNoViolations } from "jest-axe";
+import { expect } from "vitest";
+
+expect.extend(toHaveNoViolations);
