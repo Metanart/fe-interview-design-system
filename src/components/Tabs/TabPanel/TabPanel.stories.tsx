d673a8fc1b31050c3cbffd96afc0453636cae21e
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-11-07 15:09:01 +0000

    test: Added TabPanelStories

---
+++ src/components/Tabs/TabPanel/TabPanel.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { Tabs } from '../Tabs';
+import { TabPanel } from './TabPanel';
+import { Typography } from '../../Typography/Typography';
+
+const meta = {
+  title: 'Components/Tabs/TabPanel',
+  component: TabPanel,
+  parameters: {
+    layout: 'padded',
+    docs: {
+      description: {
+        component: 'TabPanel component for displaying tab panel content. Only visible when the corresponding tab is active.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof TabPanel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: 'contact',
+    children: null,
+  },
+  render: () => (
+    <Tabs 
+      defaultActiveTabId="contact" 
+      tabsGroupId="storybook-tabpanel"
+    >
+      <TabPanel id="contact">
+        <Typography variant="header-1">Contact</Typography>
+        <Typography variant="body-m">
+          Get in touch with us! We're here to help answer any questions you may have. Reach out 
+          through any of our contact channels and we'll respond as soon as possible.
+        </Typography>
+        <Typography variant="body-m">
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
+          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro 
+          quisquam est, qui dolorem ipsum quia dolor sit amet.
+        </Typography>
+      </TabPanel>
+    </Tabs>
+  ),
+};
+
